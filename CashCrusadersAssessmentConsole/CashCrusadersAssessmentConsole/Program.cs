fd5784096cad55acb19183c06d7bf1a292091a60
NotYourTypicalNerd <esauselo@gmail.com>
2023-09-14 10:11:04 +0200

    Add files via upload

--- CashCrusadersAssessmentConsole/CashCrusadersAssessmentConsole/Program.cs
+++ CashCrusadersAssessmentConsole/CashCrusadersAssessmentConsole/Program.cs
@@ -1,5 +1,15 @@
-﻿Console.WriteLine("Please enter a string:");
-var input = Console.ReadLine();
+﻿using CashCrusadersAssessmentConsole;
 
-Console.WriteLine("Please enter the character of which to find the count:");
-var character = Console.ReadKey();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a string:");
+        var input = Console.ReadLine();
+
+        Console.WriteLine("Please enter the character of which to find the count:");
+        var character = Console.ReadKey();
+
+        StringHelper.CountCharacterOccurance(character.KeyChar, input);
+    }
+}
